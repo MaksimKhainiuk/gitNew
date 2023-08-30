5db6fad6ce3b5b119c9f385b84e0db1f67490e86
MaksimKhainiuk <141557961+MaksimKhainiuk@users.noreply.github.com>
2023-08-30 11:54:49 +0300

    Add files via upload

---
+++ Tie.java
@@ -0,0 +1,34 @@
+public class Tie {
+    public static void main(String[] args) {
+        //узел
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < i + 2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 5 - 2 * i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //верхн€€ часть
+        for (int i = 0; i < 5; i++) {
+            for (int j = 3 - i; j >= 0 ; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //нижн€€ часть
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <  i + 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 7 - 2 * i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
