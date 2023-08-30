2b508106c04e0d6ea2f5c87febb98db9fd5a4b7d
MaksimKhainiuk <141557961+MaksimKhainiuk@users.noreply.github.com>
2023-08-30 12:32:45 +0300

    Add files via upload

---
+++ VowelsAtTheText.java
@@ -0,0 +1,24 @@
+package RegularExpression.taskB;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class VowelsAtTheText {
+    public  static void main(String[]args){
+        String s = "";
+        while (!s.equals("exit")) {
+            s = new Scanner(System.in).nextLine();
+            Vowels(s);
+        }
+    }
+
+    private static void Vowels(String text) {
+            Pattern p = Pattern.compile("([АОУЭИЫЕЁЯЮаоуэиыеёяю])");
+            Matcher m = p.matcher(text);
+        while(m.find()) {
+            System.out.printf("гласная буква в этом тексте: %s\n ", m.group());
+            if (m.group().isEmpty()) break;
+        }
+    }
+}
