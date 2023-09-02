0406c8da168ad230fc3156664d021c0f316684c6
MaksimKhainiuk <141557961+MaksimKhainiuk@users.noreply.github.com>
2023-09-02 11:38:59 +0300

    Add files via upload

---
+++ RomanToInteger.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    public static int romanToInteger(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        char[]sChar = s.toCharArray();
+
+        map.put('I',1);
+        map.put('V',1);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        int sum = 0;
+
+        for (int i = 0; i < s.length()-1; i++) {
+
+            if((map.get(sChar[i + 1]) <= map.get(sChar[i]))){
+                sum = sum + map.get(sChar[i]);
+            }else{
+                sum = sum - map.get(sChar[i]);
+            }
+        }
+        return  sum + map.get(sChar[s.length()-1]);
+    }
+    public static void main(String[] args) {
+        System.out.println(romanToInteger("MMXXIII"));
+    }
+
+}
