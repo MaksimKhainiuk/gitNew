13200528ad3f97ffa02088d038a4d6b78489f366
MaksimKhainiuk <maksimkorkuc2@gmail.com>
2023-09-05 19:37:17 +0300

    someChanges

---
+++ trying_to_do_something/Tie.java
@@ -0,0 +1,35 @@
+package trying_to_do_something;
+public class Tie {
+    public static void main(String[] args) {
+        //����
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < i + 2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 5 - 2 * i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //������� �����
+        for (int i = 0; i < 5; i++) {
+            for (int j = 3 - i; j >= 0 ; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //������ �����
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <  i + 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 7 - 2 * i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ trying_to_do_something/VowelsAtTheText.java
@@ -0,0 +1,25 @@
+package trying_to_do_something;
+
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class VowelsAtTheText {
+    public  static void main(String[]args){
+        String s = "";
+        while (!s.equals("exit")) {
+            s = new Scanner(System.in).nextLine();
+            Vowels(s);
+        }
+    }
+
+    private static void Vowels(String text) {
+            Pattern p = Pattern.compile("([������Ũ�����������])");
+            Matcher m = p.matcher(text);
+        while(m.find()) {
+            System.out.printf("������� ����� � ���� ������: %s\n ", m.group());
+            if (m.group().isEmpty()) break;
+        }
+    }
+}
