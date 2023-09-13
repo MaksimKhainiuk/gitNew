b62006c1251722cd921f3262ccef266b340fd539
MaksimKhainiuk <maksimkorkuc2@gmail.com>
2023-09-13 12:08:29 +0300

    windows-1251

---
+++ ReqularExpression6/taskB/VowelsAtTheText.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class VowelsAtTheText {
+    public  static void main(String[]args){
+        String s = "";
+        while (!s.equals("exit")) {
+            s = new Scanner(System.in).nextLine();
+            Vowels(s);
+        }
+    }
+
+    private static void Vowels(String text) {
+            Pattern p = Pattern.compile("([������Ũ�����������])");//windows-1251
+            Matcher m = p.matcher(text);
+        while(m.find()) {
+            System.out.printf("������� ����� � ���� ������: %s\n ", m.group());//windows1251
+            if (m.group().isEmpty()) break;
+        }
+    }
+}
