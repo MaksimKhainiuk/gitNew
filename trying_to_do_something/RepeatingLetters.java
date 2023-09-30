4b5654a1a1c836255cf7ba5ce3351a10f98682d8
MaksimKhainiuk <maksimkorkuc2@gmail.com>
2023-09-30 17:17:22 +0300

    aComment

--- trying_to_do_something/RepeatingLetters.java
+++ trying_to_do_something/RepeatingLetters.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class RepeatingLetters {
     public static void main(String[] args) {
-        System.out.println(isUnique("1234567"));
-        System.out.println(isUnique("abcade"));
-        System.out.println(isUnique("abcABC"));
-        System.out.println(isUnique("aaA"));
+        System.out.println(isUnique("1234567")); //-> true
+        System.out.println(isUnique("abcade")); //-> false
+        System.out.println(isUnique("abcABC")); //-> true
+        System.out.println(isUnique("aaA")); //-> false
     }
     private static boolean isUnique(String text){
         Set<Character>set = new HashSet<>();
