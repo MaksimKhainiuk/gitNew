799cca3e958334f2f59b783c666832a266fa7471
MaksimKhainiuk <maksimkorkuc2@gmail.com>
2023-10-06 11:15:23 +0300

    additionFizzBuzz

--- trying_to_do_something/FizzBuzz.java
+++ trying_to_do_something/FizzBuzz.java
@@ -6,9 +6,11 @@ public class FizzBuzz {
     public static void main(String[] args) {
         System.out.print("Enter an integer: ");
         int n = new Scanner(System.in).nextInt();
-        System.out.println(fizzBizz(n));
+        System.out.println(fizzBuzz(n));
+        System.out.println();
+        System.out.println(fizzBuzz2(n));
     }
-    private static List<String> fizzBizz(int n){
+    private static List<String> fizzBuzz(int n){
         List<String>answer = new ArrayList<>();
         for (int i = 1; i < n; i++) {
             boolean divisibleBy3 = i % 3 == 0;
@@ -21,6 +23,27 @@ public class FizzBuzz {
             } else if (divisibleBy5) {
                 answer.add("Buzz");
             }else answer.add(String.valueOf(i));
+        }
+        return answer;
+    }
+    private static List<String> fizzBuzz2(int n){
+        List<String>answer = new ArrayList<>();
+        for (int i = 1; i < n; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            String currStr = "";
+
+            if(divisibleBy3){
+                currStr += "Fizz";
+            }
+            if(divisibleBy5){
+                currStr += "Buzz";
+            }
+            if(currStr.isEmpty()){
+                currStr += String.valueOf(i);
+            }
+            answer.add(currStr);
         }
         return answer;
     }
